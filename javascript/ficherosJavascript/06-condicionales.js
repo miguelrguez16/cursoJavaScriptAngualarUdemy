efd1fa41c6c222852d784e1386246795d929636d
Miguel <uo263494@uniovi.es>
2022-01-31 16:23:27 +0100

    bucles

--- javascript/ficherosJavascript/06-condicionales.js
+++ javascript/ficherosJavascript/06-condicionales.js
@@ -1,8 +1,8 @@
-'use strict'
-console.log("Vinculado 06-condicionales")
+"use strict";
+console.log("Vinculado 06-condicionales");
 
 if (4 > 0) {
-    console.log("4 es mayor jeje");
+  console.log("4 es mayor jeje");
 }
 /**
  * mayor >
@@ -14,3 +14,24 @@ if (4 > 0) {
  * igualdad estricta ===
  * suma 1: +=
  */
+
+// Operadores logicos
+/**
+ * && AND
+ * || OR
+ * ! NOT
+ */
+var edad = 19;
+// EN UNS SWITCH LOS OPERADORES LOGICOS NO FUNCIONAN
+// estructura switch
+switch (edad) {
+  case 18:
+    console.log("Mayor de 18");
+    break;
+  case 5:
+    console.log("Menor de 18");
+    break;
+  default:
+    console.log("Error dato");
+    break;
+}
---
+++ javascript/ficherosJavascript/08-bucles.js
@@ -0,0 +1,5 @@
+"use strict";
+for (let i = 1; i <= 2; i++) {
+  console.log(i);
+  debugger; // invoca la funcionalidad de debuguear en esa linea de codigo si está disponible
+}

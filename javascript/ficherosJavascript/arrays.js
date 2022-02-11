4488a234694ff9ecb3312309ca07293e6bbe9a63
Miguel <uo263494@uniovi.es>
2022-02-11 15:40:09 +0100

    fin eje array

--- javascript/ficherosJavascript/arrays.js
+++ javascript/ficherosJavascript/arrays.js
@@ -1,29 +1,54 @@
 "use strict";
 
-var vtr = new Array(1,2,3,4,5,65,2);
+var vtr = new Array(1, 2, 3, 4, 5, 65, 2);
 
 document.write("</br><ul>");
-vtr.forEach((elemento,indice) => {
-    document.write("<li>[" +indice+"]:" + elemento + "</li>");
+vtr.forEach((elemento, indice) => {
+  document.write("<li>[" + indice + "]:" + elemento + "</li>");
 });
 document.write("</ul>");
 
-
 var categorias = ["Accion", "Comedia", "Terror", "Alfajores"];
-var peli = ["le vi est bell","Kingsman","AVatar","El señor de los anillos"];
-var matriz = [categorias,peli];
+var peli = ["le vie est bell", "Kingsman", "AVatar", "El señor de los anillos"];
+var matriz = [categorias, peli];
 console.log(matriz);
-matriz.push("batman"); //metes en el array
+matriz.push("batman"); // * metes en el array
 console.log(matriz);
-var ele = matriz.pop();//sacas y elimninas el ultimo
+var ele = matriz.pop(); // * sacas y elimninas el ultimo
 
 console.log(ele);
 
 console.log(matriz);
 console.log(vtr);
 
-
-vtr.splice(2,1); // te elina el que tu quieres
+vtr.splice(2, 1); // te elina el que tu quieres
 console.log(vtr);
-// CONVERTIR EL ARRAY En un string
+// * CONVERTIR EL ARRAY En un string
 console.log(vtr.join());
+
+// * ORDENAR VECTOR
+console.log("DEL REVES")
+vtr.sort((a,b)=> b-a);
+console.log(vtr.join()); 
+
+// *  DARLE LA VUELTA
+vtr = vtr.reverse();
+console.log(vtr.join());
+
+// * FOR IN
+console.log("FOR IN");
+for(let palabra in categorias){
+    console.log(`
+        "Palabra[${palabra}] = ${categorias}"
+        `);
+}
+
+
+// * BUSQUEDAS EN ARRAY
+var busquedaPalabra = categorias.findIndex(categoria => categoria==="Comedia");
+console.log(busquedaPalabra);
+
+// * EXISTEN VALORES QUE CUMPLEN ALGUNA CONDICION
+// * Busqueda comprobacion si hay valores
+var busquedaNumero = vtr.some(numBuscar => numBuscar>3);
+console.log(busquedaNumero);
---
+++ javascript/ficherosJavascript/ejercicioArrays.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/**
+ * 1. Pida 6 numero por pantalla y los meta en un array
+ * 2. Mostar el array entero en el cuerpo de la pag y consola
+ * 3. Mostrar el array ordenado
+ * 4. Invertir su orden y mostrarlo
+ * 5. Mostar cuantos elementos tiene
+ * 6. Busqueda de un valor introducido por el usuario
+ *  Si está que nos diga el indice
+ */
+
+var numImput = 0;
+var cont = 0;
+var vector=[];
+
+function documenta(titulo, vector) {
+  document.write(`<h2>${titulo}</h2>`);
+  document.write("<lu>");
+  vector.forEach((element, index) => {
+    document.write("<li>[" + index + "]:" + element + "</li>");
+  });
+  document.write("</lu>");
+}
+
+while (cont < 6) {
+  numImput = parseFloat(prompt("Introduce valor numérico: "));
+  if (!isNaN(numImput)) {
+    vector.push(numImput);
+    cont++;
+  }
+}
+// ? 1.Mostar vector, documento y consola
+var titu = "vector intro";
+documenta(titu, vector);
+console.log("Vector obtenido ", vector.join());
+// ? VECTOR ORDENADO
+documenta(
+  "Vector ordenado",
+  vector.sort((a, b) => a - b)
+);
+// ? VECTOR AL REVES
+documenta(
+  "Vector al reves",
+  vector.sort((a, b) => b - a)
+);
+console.log();
+// ? Número de elementos
+document.write(`<h2>Longitud ${vector.length}</h2>`);
+
+// ? Encontrar posicion numero
+var posicionNumeroBusqueda = vector.indexOf(parseFloat(prompt("Número a buscar: ")));
+if(posicionNumeroBusqueda==-1){
+    document.write("<p>Número:  no encontrado</p>")
+}else {
+    document.write(`<p>Número encontrado, posicion ${posicionNumeroBusqueda}</p>`);
+}

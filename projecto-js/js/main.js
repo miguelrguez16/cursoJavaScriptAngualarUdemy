3718a2036abff7186cb5d919e6f9d9a2b162771d
Miguel <uo263494@uniovi.es>
2022-02-18 23:31:10 +0100

    maquetacion y plugin

---
+++ projecto-js/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+$(document).ready(function () {
+    //console.log("cargado");
+
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200,
+        pager: false
+      });
+});

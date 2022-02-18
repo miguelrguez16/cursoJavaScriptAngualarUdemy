a2278b931617a7bd277106f03dfbbadb85122515
Miguel <uo263494@uniovi.es>
2022-02-18 11:06:14 +0100

    inciio jquery ui

--- aprendiendo_jquery/js/06-interface.js
+++ aprendiendo_jquery/js/06-interface.js
@@ -2,4 +2,25 @@
 
 $(document).ready(function () {
   console.log("Cargado 06-interface.js");
+
+    // ! Moverlos 
+    $(".elemento").draggable();
+
+    // ! resizable
+    $(".elemento").resizable();
+
+
+    // ! seleccionar elmentos
+   // $(".lista_seleccionable").selectable();
+
+    //! Ordenar elementos
+    $(".lista_seleccionable").sortable();
+
+    $("#save").click(function(){
+        $("li").each(function(index, element){
+            console.log(element.innerHTML);
+            //console.log(index);
+        })
+    })
+
 });
